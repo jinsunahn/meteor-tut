4dd4a48ac86fa102fed9f6c9a025529d37dfabc8
Jin Sun Ahn <jin@qwertyer.com>
2015-05-29 13:28:04 -0700

    Wired collection into postsList template

--- vinple/client/templates/posts/posts_list.js
+++ vinple/client/templates/posts/posts_list.js
@@ -1,9 +1,5 @@
-var postsData = [
-  {
-    title: 'Introducing Vinple',
-    url: 'http://vinple.com'
-  }
-];
 Template.postsList.helpers({
-  posts: postsData
+  posts: function() {
+    return Posts.find();
+  }
 });
