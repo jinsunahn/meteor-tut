df654575c885f96916cf96fa4423dad207e86dd0
Jin Sun Ahn <jin@qwertyer.com>
2015-06-05 20:57:49 -0700

    Actually use the error reporting

--- vinple/client/templates/posts/post_item.js
+++ vinple/client/templates/posts/post_item.js
@@ -1,10 +1,31 @@
-Template.postItem.helpers({
-  ownPost: function() {
-    return this.userId === Meteor.userId();
+Template.postEdit.events({
+  'submit form': function(e) {
+    e.preventDefault();
+    
+    var currentPostId = this._id;
+    
+    var postProperties = {
+      url: $(e.target).find('[name=url]').val(),
+      title: $(e.target).find('[name=title]').val()
+    }
+    
+    Posts.update(currentPostId, {$set: postProperties}, function(error) {
+      if (error) {
+        // display the error to the user
+        throwError(error.reason);
+      } else {
+        Router.go('postPage', {_id: currentPostId});
+      }
+    });
   },
-  domain: function() {
-    var a = document.createElement('a');
-    a.href = this.url;
-    return a.hostname;
+  
+  'click .delete': function(e) {
+    e.preventDefault();
+    
+    if (confirm("Delete this post?")) {
+      var currentPostId = this._id;
+      Posts.remove(currentPostId);
+      Router.go('postsList');
+    }
   }
 });
