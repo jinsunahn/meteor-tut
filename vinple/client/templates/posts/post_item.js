7fddb84eafd47a0214a4ee83b4f32bec7bdf5282
Jin Sun Ahn <jin@qwertyer.com>
2015-06-08 23:39:21 -0700

    Display comments on postPage

--- vinple/client/templates/posts/post_item.js
+++ vinple/client/templates/posts/post_item.js
@@ -1,31 +1,13 @@
-Template.postEdit.events({
-  'submit form': function(e) {
-    e.preventDefault();
-    
-    var currentPostId = this._id;
-    
-    var postProperties = {
-      url: $(e.target).find('[name=url]').val(),
-      title: $(e.target).find('[name=title]').val()
-    }
-    
-    Posts.update(currentPostId, {$set: postProperties}, function(error) {
-      if (error) {
-        // display the error to the user
-        throwError(error.reason);
-      } else {
-        Router.go('postPage', {_id: currentPostId});
-      }
-    });
+Template.postItem.helpers({
+  ownPost: function() {
+    return this.userId == Meteor.userId();
   },
-  
-  'click .delete': function(e) {
-    e.preventDefault();
-    
-    if (confirm("Delete this post?")) {
-      var currentPostId = this._id;
-      Posts.remove(currentPostId);
-      Router.go('postsList');
-    }
+  domain: function() {
+    var a = document.createElement('a');
+    a.href = this.url;
+    return a.hostname;
+  },
+  commentsCount: function() {
+    return Comments.find({postId: this._id}).count();
   }
 });
