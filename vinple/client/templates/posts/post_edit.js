b0c58ba1cd8238bd6f955e7731a7bf1148b6c537
Jin Sun Ahn <jin@qwertyer.com>
2015-06-10 20:15:27 -0700

    Added routes for post lists, and pages to display them

--- vinple/client/templates/posts/post_edit.js
+++ vinple/client/templates/posts/post_edit.js
@@ -29,7 +29,7 @@ Template.postEdit.events({
     Posts.update(currentPostId, {$set: postProperties}, function(error) {
       if (error) {
         // display the error to the user
-        Errors.throw(error.reason);
+        throwError(error.reason);
       } else {
         Router.go('postPage', {_id: currentPostId});
       }
@@ -42,7 +42,7 @@ Template.postEdit.events({
     if (confirm("Delete this post?")) {
       var currentPostId = this._id;
       Posts.remove(currentPostId);
-      Router.go('postsList');
+      Router.go('home');
     }
   }
 });
